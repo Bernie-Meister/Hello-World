73dd203bcb4d6d020b9de201a540d25ea5246a15
Bernardo E. Dramos Jr <63249192+Bernie-Meister@users.noreply.github.com>
2022-03-21 09:34:55 +0200

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,10 @@
+# Hello-World
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, \n")
+    printf("I love the computing world, and hope to make as many contributions as I can. \n")
+    printf("I hope to share great experiences and journeys with you.")
+}
--- README.md
+++
@@ -1,7 +0,0 @@
-# Hello-World
-
-Hello and welcome...
-
-I am one of many computer enthusiasts, although I'm not proficient in my skills.
-I played golf on Pluto, Twas a good lightyear...
-
